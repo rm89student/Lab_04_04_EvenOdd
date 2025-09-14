2bf365c37f7691aeebfe01fcb37d2f974aedd307
rm89student <rmille6r8@mail.uc.edu>
2025-09-14 15:57:52 -0400

    Initial commit

---
+++ src/Lab_04_04_EvenOdd.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Lab_04_04_EvenOdd {
+    public static void main(String[] args) {
+       int numToExamine = 2;
+
+       if (numToExamine % 2 == 0) {
+           System.out.println(numToExamine + "is Even.");
+       } else {
+           System.out.println(numToExamine + "is Odd.");
+        }
+    }
+}
